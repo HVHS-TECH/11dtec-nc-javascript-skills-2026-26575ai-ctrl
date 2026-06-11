c8b6bada29e3da268dc9140a860c9286ea3a051a
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-11 21:54:05 +0000

    Jun 11, 2026, 9:54 PM

--- t03_maths.js
+++ t03_maths.js
@@ -8,7 +8,7 @@ var name ="Airi"
 var age=16;
 var isStudent=true;
 let pocketMoney=6;
-var born=japan;
+//var born=japan;
 var num1=10;
 var num2=16;
 var num3=pocketMoney/2
@@ -16,7 +16,7 @@ var num3=pocketMoney/2
 
 console.log("Hi"+name);
 console.log("As of you are "+age+"years old");
-console.log("You were born in"+born);
+//console.log("You were born in"+born);
 console.log("In 10 years you will be"+num1+num2);
 console.log("You have"+pocketMoney+"dollars");
 console.log("You spend half of your money, now you have"+pocketmoney/2);
