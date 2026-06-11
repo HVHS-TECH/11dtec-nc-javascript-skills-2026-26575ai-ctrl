85ff86ff77028c16816a227954ac735db1559c0e
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-11 21:52:22 +0000

    Jun 11, 2026, 9:52 PM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,23 @@
-/* Paste your code from task 1 here */
+/****************************
+Name of the task:
+****************************/
+console.log("Running t01_introduction.js")
+
+//Variables
+var name ="Airi"
+var age=16;
+var isStudent=true;
+let pocketMoney=3;
+console.log(pocketMoney);
+console.log("You have"+pocketMoney+"dollars");
+
+/****************************
+Main code
+****************************/
+
+
+
+
+/****************************
+Functions
+****************************/
--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,35 @@
-/* Paste your code from task 2 here */
+/****************************
+Name of the task:
+****************************/
+console.log("Running t01_introduction.js")
+
+//Variables
+var name ="Airi"
+var age=16;
+var isStudent=true;
+let pocketMoney=6;
+var born=japan;
+var num1=10;
+var num2=16;
+var num3=pocketMoney/2
+
+
+console.log("Hi"+name);
+console.log("As of you are "+age+"years old");
+console.log("You were born in"+born);
+console.log("In 10 years you will be"+num1+num2);
+console.log("You have"+pocketMoney+"dollars");
+console.log("You spend half of your money, now you have"+pocketmoney/2);
+console.log("Then you get $3, now you have"+num3);
+
+
+/****************************
+Main code
+****************************/
+
+
+
+
+/****************************
+Functions
+****************************/
