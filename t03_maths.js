b45c37b61398dcc8dd5431e171905a93c600dd5d
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-14 22:49:01 +0000

    Jun 14, 2026, 10:49 PM

--- t03_maths.js
+++ t03_maths.js
@@ -15,18 +15,18 @@ var num2=16;
 var num3=pocketMoney/2;
 var num4=num3+3;
 
-
-
-
-/****************************
-Main code
-****************************/
 console.log("Hi"+name);
 console.log("As of you are "+age+"years old");
 console.log("In 10 years you will be"+num1+num2);
 console.log("You have"+pocketMoney+"dollars");
 console.log("You spend half of your money, now you have"+num3);
 console.log("Then you get $3, now you have"+num4);
+
+
+/****************************
+Main code
+****************************/
+
 
 
 
