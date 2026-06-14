b7d949ae72f41673c341071fd0d8cd57639a62f2
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-14 22:47:30 +0000

    Jun 14, 2026, 10:47 PM

--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,43 @@
-/* Paste your code from task 5 here */
+/****************************
+Name of the task:
+****************************/
+console.log("Running t01_introduction.js")
+const OUTPUT = document.getElementById("JavaScriptOutput");
+OUTPUT.innerHTML ="<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML +="<p>Hello"+ userName + "</p>";
+OUTPUT.innerHTML +="<p>Second Paragraph line.</p>";
+OUTPUT.innerHTML +="<p>Therd Paragraph line.</p>";
+
+//Variables
+var name ="Airi"
+var age=16;
+var isStudent=true;
+let pocketMoney=6;
+//var born=japan;
+var num1=10;
+var num2=16;
+var num3=pocketMoney/2;
+var num4=num3+3;
+
+
+
+
+/****************************
+Main code
+****************************/
+console.log("Hi"+name);
+console.log("As of you are "+age+"years old");
+console.log("In 10 years you will be"+num1+num2);
+console.log("You have"+pocketMoney+"dollars");
+console.log("You spend half of your money, now you have"+num3);
+console.log("Then you get $3, now you have"+num4);
+
+
+
+/****************************
+Functions
+****************************/
+function writeLine(){
+    //Add a line to the html page
+    OUTPUT.innerHTML +="<p>Less code matters</p>";
+}
