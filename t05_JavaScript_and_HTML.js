95b3952067b907d77715cb6a2610966e62c53459
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-15 23:49:56 +0000

    Jun 15, 2026, 11:49 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,38 +1,19 @@
-/****************************
-Name of the task:
-****************************/
-<!DOCTYPE html>
+//<!DOCTYPE html>
 <html>
 <body>
-console.log("Running t01_introduction.js")
+<h1>JavaScript</h1>
+<h2>The onclick Event</h2>
+<p id="demo">Click the button to change the layout of this paragraph</p>
 
-
-//Variables
-var name ="Airi"
-var age=16;
-var isStudent=true;
-let pocketMoney=6;
-var num1=10;
-var num2=16;
-var num3=pocketMoney/2;
-var num4=num3+3;
-
-console.log("Hi"+name);
-console.log("As of you are "+age+"years old");
-console.log("In 10 years you will be"+num1+num2);
-console.log("You have"+pocketMoney+"dollars");
-console.log("You spend half of your money, now you have"+num3);
-console.log("Then you get $3, now you have"+num4);
-
-
-
-<p>Bla bla bla</p>
+<button onclick="myFunction()">Click Me!</button>
 
 <script>
-document.write(Date());
+function myFunction() {
+  let x = document.getElementById("demo");
+  x.style.fontSize = "25px"; 
+  x.style.color = "red"; 
+}
 </script>
-
-<p>Bla bla bla</p>
 
 </body>
 </html>
