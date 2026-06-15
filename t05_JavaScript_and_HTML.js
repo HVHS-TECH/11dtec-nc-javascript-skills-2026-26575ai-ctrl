cb33e1bda5a65d8d0a1b39a81978cfde52b3f19b
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-15 23:58:19 +0000

    Jun 15, 2026, 11:58 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -13,3 +13,39 @@ function myFunction() {
 }
 
 document.getElementById('btn').addEventListener('click', myFunction);
+/****************************
+Name of the task:
+****************************/
+
+console.log("Running t01_introduction.js")
+
+
+//Variables
+var name ="Airi"
+var age=16;
+var isStudent=true;
+let pocketMoney=6;
+var num1=10;
+var num2=16;
+var num3=pocketMoney/2;
+var num4=num3+3;
+
+console.log("Hi"+name);
+console.log("As of you are "+age+"years old");
+console.log("In 10 years you will be"+num1+num2);
+console.log("You have"+pocketMoney+"dollars");
+console.log("You spend half of your money, now you have"+num3);
+console.log("Then you get $3, now you have"+num4);
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Bla bla bla</p>
+
+<script>
+document.write(Date());
+</script>
+
+</body>
+</html>
