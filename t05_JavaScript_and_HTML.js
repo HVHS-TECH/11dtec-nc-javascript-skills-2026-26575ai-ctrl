79ec812861208c7c9a231183e314073943ccdd6c
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-16 00:04:36 +0000

    Jun 16, 2026, 12:04 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,18 +1,4 @@
-// This file produces a small HTML snippet and behavior when loaded in a browser environment.
-document.body.innerHTML = `
-  <h1>JavaScript</h1>
-  <h2>The onclick Event</h2>
-  <p id="demo">Click the button to change the layout of this paragraph</p>
-  <button id="btn">Click Me!</button>
-`;
 
-function myFunction() {
-  const x = document.getElementById("demo");
-  x.style.fontSize = "25px";
-  x.style.color = "red";
-}
-
-document.getElementById('btn').addEventListener('click', myFunction);
 /****************************
 Name of the task:
 ****************************/
