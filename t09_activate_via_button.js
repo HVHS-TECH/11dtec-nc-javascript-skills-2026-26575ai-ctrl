5f14a0dad2652a101f2af398cb40e1f4ea6523ad
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-18 21:28:13 +0000

    Jun 18, 2026, 9:28 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -23,7 +23,8 @@ OUTPUT.innerHTML+="<p>You have"+pocketMoney+"dollars</p>";
 OUTPUT.innerHTML+="<p>You spend half of your money, now you have"+num3+"</p>";
 OUTPUT.innerHTML+="<p>Then you get $3, now you have"+num4+"</p>";
 
-
+<p>Open the console <code>(Ctrl-Shift-i)</code>>
+<button> oneclick="start()"></button></>
 /****************************
 Main code
 ****************************/
