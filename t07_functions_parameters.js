99c42f859174fc9c8febad0e46cab3d6666ddc4e
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-18 21:11:14 +0000

    Jun 18, 2026, 9:11 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -46,19 +46,3 @@ function displayWelcome(_name,_age,_chocolateBar){
     OUTPUT.innerHTML+=`<p>Chocolate bar: ${_chocolateBar}</p>`;
 
 }
-var myVar=0;
-OUTPUT.innerHTML="Start: " + myVar+"<br>";
-
-functionOne();
-function functionOne(){
-    myVar+1;
-    OUTPUT.innerHTML+="Function one: " + myVar+"<br>";
-}
-
-functionTwo();
-function functionTwo(){
-    myVar+2;
-    OUTPUT.innerHTML+="Function two: " + myVar+"<br>";
-}
-
-OUTPUT.innerHTML+="end: " + myVar+"<br>";
--- t08_scope.js
+++ t08_scope.js
@@ -1,24 +1,21 @@
 const output = document.getElementById("output");
 
-/* Read the slides, make a prediction, then uncomment this code.
+// Read the slides, make a prediction, then uncomment this code.
 
-
-var myVar = 0;
-output.innerHTML = "Start: "+myVar+"<br>";
+var myVar=0;
+OUTPUT.innerHTML="Start: " + myVar+"<br>";
 
 functionOne();
-function functionOne() {
-  myVar = 1;
-  output.innerHTML += "functionOne: "+myVar+"<br>";
+function functionOne(){
+    myVar+1;
+    OUTPUT.innerHTML+="Function one: " + myVar+"<br>";
 }
 
 functionTwo();
-function functionTwo() {
-  var myVar = 2;
-  output.innerHTML += "functionTwo: "+myVar+"<br>";
+function functionTwo(){
+    myVar+2;
+    OUTPUT.innerHTML+="Function two: " + myVar+"<br>";
 }
 
-output.innerHTML += "end: "+myVar+"<br>";
+OUTPUT.innerHTML+="end: " + myVar+"<br>";
 
-
-*/
