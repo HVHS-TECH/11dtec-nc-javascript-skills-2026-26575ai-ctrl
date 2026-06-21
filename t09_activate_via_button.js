12c4293abd8787064d494a64c177809811fa7bff
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-21 22:27:11 +0000

    Jun 21, 2026, 10:27 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -14,17 +14,16 @@ var num3=pocketMoney/2;
 var num4=num3+3;
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-OUTPUT.innerHTML="<h2>Added by JavaScript</h2>";
-OUTPUT.innerHTML="<p>Hi "+name+"</p>";
-OUTPUT.innerHTML+="<p>As of you are "+age+"years old</p>";
-OUTPUT.innerHTML+="<p>In 10 years you will be"+num1+num2+"</p>";
-OUTPUT.innerHTML+="<p>You have"+pocketMoney+"dollars</p>";
-OUTPUT.innerHTML+="<p>You spend half of your money, now you have"+num3+"</p>";
-OUTPUT.innerHTML+="<p>Then you get $3, now you have"+num4+"</p>";
+if (OUTPUT) {
+    OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+    OUTPUT.innerHTML += "<p>Hi " + name + "</p>";
+    OUTPUT.innerHTML += "<p>As of you are " + age + " years old</p>";
+    OUTPUT.innerHTML += "<p>In 10 years you will be " + (num1 + num2) + "</p>";
+    OUTPUT.innerHTML += "<p>You have " + pocketMoney + " dollars</p>";
+    OUTPUT.innerHTML += "<p>You spend half of your money, now you have " + num3 + "</p>";
+    OUTPUT.innerHTML += "<p>Then you get $3, now you have " + num4 + "</p>";
+}
 
-<p>Open the console <code>(Ctrl-shift-i)</code></p>
-<button onclick="start()">Start</button>
-<div id="spaceForJavaScriptOutput"></div>
 /****************************
 Main code
 ****************************/
