4b67e4a8da8bad01feefa8d9bb3ecbde999e8420
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-22 23:44:14 +0000

    Jun 22, 2026, 11:44 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -2,7 +2,7 @@
 Name of the task:
 ****************************/
 const OUTPUT = document.getElementById("JavaScriptOutput");
-OUTPUT.innerHTML = "HEllo World";
+OUTPUT.innerHTML = "Hello World";
 const NAME_FIELD = document.getElementById("nameField");
 let userName = NAME_FIELD.value;
 console.log("Running t01_introduction.js")
