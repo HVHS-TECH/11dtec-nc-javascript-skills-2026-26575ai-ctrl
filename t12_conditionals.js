901e2e29ad9bb6ebaaba09a6fa8d9cf9331b3cad
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-23 00:19:41 +0000

    Jun 23, 2026, 12:19 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -5,12 +5,16 @@ console.log("Running t12_conditionals.js")
 
 var name ="Airi"
 var age=16;
-var isStudent=true;
 
 /****************************
 Main code
 ****************************/
 
+if(age < 18) {
+    console.log("You are not an adult.");
+} else {
+    console.log("You are an adult.");
+}
 
 /****************************
 Functions
