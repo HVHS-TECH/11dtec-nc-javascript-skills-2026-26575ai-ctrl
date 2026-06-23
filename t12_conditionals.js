b903d3f4fec2ba4abe9b2d0962e6c370794b1560
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-23 00:23:39 +0000

    Jun 23, 2026, 12:23 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,21 +1,49 @@
 /****************************
 Name of the task:
 ****************************/
-console.log("Running t12_conditionals.js")
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "Hello World";
+const NAME_FIELD = document.getElementById("nameField");
+let userName = NAME_FIELD.value;
+console.log("Running t11_data_types.js")
 
-var name ="Airi"
-var age=16;
+//Variables
+var drivingAge = 16;
+var schoolName = "HVHS";
+var validReply = false;
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+if (OUTPUT) {
+    OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+    OUTPUT.innerHTML += "<p>Hi " + name + "</p>";
+    OUTPUT.innerHTML += "<p>As of you are " + age + " years old</p>";
+    OUTPUT.innerHTML += "<p>In 10 years you will be " + (num1 + num2) + "</p>";
+    OUTPUT.innerHTML += "<p>You have " + pocketMoney + " dollars</p>";
+    OUTPUT.innerHTML += "<p>You spend half of your money, now you have " + num3 + "</p>";
+    OUTPUT.innerHTML += "<p>Then you get $3, now you have " + num4 + "</p>";
+}
 
 /****************************
 Main code
 ****************************/
 
-if(age < 18) {
-    console.log("You are not an adult.");
-} else {
-    console.log("You are an adult.");
-}
+
+
 
 /****************************
 Functions
 ****************************/
+function start(){
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer=currentYear - userAge;
+    spaceForJavaScriptOutput.innerHTML
+}
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML ="<p>Your name is " + userName + "</p>";
+}
+function Number(){
+    let userAgeString = AGE_FIELD.value;
+    let userAgeNumber 
