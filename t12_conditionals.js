d989e96bcdf2b18fe89e9167c6b9beac6e1c56c7
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-25 21:32:15 +0000

    Jun 25, 2026, 9:32 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -23,7 +23,7 @@ if (OUTPUT) {
     OUTPUT.innerHTML += "<p>You spend half of your money, now you have " + num3 + "</p>";
     OUTPUT.innerHTML += "<p>Then you get $3, now you have " + num4 + "</p>";
 }
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const OUTPUT = document.getElementById("t12_conditionals");
 if(pocketMoney < 4){
     OUTPUT.innerHTML += "<p>A chocolate bar costs $4 Sorry you can't afford it.</p>";
 }else if(pocketMoney >= 4){
