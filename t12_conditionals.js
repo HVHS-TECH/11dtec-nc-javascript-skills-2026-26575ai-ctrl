deff44cbeda459316b742513b693ebf84667f17f
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-25 21:53:11 +0000

    Jun 25, 2026, 9:53 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -4,15 +4,7 @@ Name of the task:
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 const NAME_FIELD = document.getElementById("nameField");
 const AGE_FIELD = document.getElementById("ageField");
-function getFormInput(spaceForJavaScriptOutput){
-let userName = NAME_FIELD.value;
-let userAge = AGE_FIELD.value;
 
-OUTPUT.innerHTML ="<h2>Added by JavaScript</h2>";
-OUTPUT.innerHTML +="<p>Your name is " + userName + "</p>";
-OUTPUT.innerHTML +="<p>Your age is " + userAge + "</p>";
-OUTPUT.innerHTML +="<p>In 10 years you will be " + (Number(userAge) + 10) + "</p>";
-}
 
 console.log("Running t12_conditionals.js")
 
@@ -34,18 +26,3 @@ Main code
 /****************************
 Functions
 ****************************/
-function start(){
-    spaceForJavaScriptOutput.innerHTML
-    spaceForJavaScriptOutput.innerHTML
-    answer=currentYear - userAge;
-    spaceForJavaScriptOutput.innerHTML
-}
-function getFormInput(){
-    const NAME_FIELD = document.getElementById("nameField");
-    let userName = NAME_FIELD.value;
-    OUTPUT.innerHTML ="<p>Your name is " + userName + "</p>";
-}
-function Number(){
-    let userAgeString = AGE_FIELD.value;
-    let userAgeNumber = Number(userAgeString);
-}
