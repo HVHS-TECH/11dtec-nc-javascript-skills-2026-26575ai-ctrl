e071fcd97d60abf664744d22cfc10f6b87610e89
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-25 21:47:14 +0000

    Jun 25, 2026, 9:47 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -7,6 +7,11 @@ const AGE_FIELD = document.getElementById("ageField");
 function getFormInput(){
 let userName = NAME_FIELD.value;
 let userAge = AGE_FIELD.value;
+
+OUTPUT.innerHTML ="<h2>Added by JavaScript</h2>";
+OUTPUT.innerHTML +="<p>Your name is " + userName + "</p>";
+OUTPUT.innerHTML +="<p>Your age is " + userAge + "</p>";
+OUTPUT.innerHTML +="<p>In 10 years you will be " + (Number(userAge) + 10) + "</p>";
 }
 
 console.log("Running t12_conditionals.js")
@@ -16,7 +21,7 @@ var drivingAge = 16;
 var schoolName = "HVHS";
 var validReply = false;
 
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+/*const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 if (OUTPUT) {
     OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
     OUTPUT.innerHTML += "<p>Hi " + userName + "</p>";
@@ -25,7 +30,7 @@ if (OUTPUT) {
     OUTPUT.innerHTML += "<p>You have " + pocketMoney + " dollars</p>";
     OUTPUT.innerHTML += "<p>You spend half of your money, now you have " + num3 + "</p>";
     OUTPUT.innerHTML += "<p>Then you get $3, now you have " + num4 + "</p>";
-}
+}*/
 
 /****************************
 Main code
