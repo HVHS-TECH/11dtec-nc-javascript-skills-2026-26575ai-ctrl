1d67ef43e06bc1f65e3e6e77b9da1791fbc37e00
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-25 21:51:52 +0000

    Jun 25, 2026, 9:51 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -4,7 +4,7 @@ Name of the task:
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 const NAME_FIELD = document.getElementById("nameField");
 const AGE_FIELD = document.getElementById("ageField");
-function getFormInput(){
+function getFormInput(spaceForJavaScriptOutput){
 let userName = NAME_FIELD.value;
 let userAge = AGE_FIELD.value;
 
@@ -22,15 +22,7 @@ var schoolName = "HVHS";
 var validReply = false;
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-if (OUTPUT) {
-    OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
-    OUTPUT.innerHTML += "<p>Hi " + userName + "</p>";
-    OUTPUT.innerHTML += "<p>As of you are " + age + " years old</p>";
-    OUTPUT.innerHTML += "<p>In 10 years you will be " + (num1 + num2) + "</p>";
-    OUTPUT.innerHTML += "<p>You have " + pocketMoney + " dollars</p>";
-    OUTPUT.innerHTML += "<p>You spend half of your money, now you have " + num3 + "</p>";
-    OUTPUT.innerHTML += "<p>Then you get $3, now you have " + num4 + "</p>";
-}
+
 
 /****************************
 Main code
