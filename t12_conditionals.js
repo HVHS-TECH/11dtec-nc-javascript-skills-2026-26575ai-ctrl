d680cec2079c5dffcc65c84929fc667e2d54f112
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-28 22:28:44 +0000

    Jun 28, 2026, 10:28 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,2 +1,50 @@
+/****************************
+Name of the task:
+****************************/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "Hello World";
+const NAME_FIELD = document.getElementById("nameField");
+let userName = NAME_FIELD.value;
+console.log("Running t11_data_types.js")
+
+//Variables
+let name = "Airi";
+let age = 16;
+let isAwake = true;
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+if (OUTPUT) {
+    OUTPUT.innerHTML = "<h2>Added by JavaScript</h2>";
+    OUTPUT.innerHTML += "<p>Hi " + name + "</p>";
+    OUTPUT.innerHTML += "<p>As of you are " + age + " years old</p>";
+    OUTPUT.innerHTML += "<p>In 10 years you will be " + (num1 + num2) + "</p>";
+    OUTPUT.innerHTML += "<p>You have " + pocketMoney + " dollars</p>";
+    OUTPUT.innerHTML += "<p>You spend half of your money, now you have " + num3 + "</p>";
+    OUTPUT.innerHTML += "<p>Then you get $3, now you have " + num4 + "</p>";
+}
+
+/****************************
+Main code
+****************************/
 
 
+
+
+/****************************
+Functions
+****************************/
+function start(){
+    spaceForJavaScriptOutput.innerHTML
+    spaceForJavaScriptOutput.innerHTML
+    answer=currentYear - userAge;
+    spaceForJavaScriptOutput.innerHTML
+}
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML ="<p>Your name is " + userName + "</p>";
+}
+function Number(){
+    let userAgeString = AGE_FIELD.value;
+    let userAgeNumber = Number(userAgeString);
+}
