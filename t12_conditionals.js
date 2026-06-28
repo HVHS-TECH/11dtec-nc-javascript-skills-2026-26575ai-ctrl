647d7301481b2a2acdc1c60eac1bfec282d45fd8
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-28 22:19:38 +0000

    Jun 28, 2026, 10:19 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,33 +1,7 @@
-/****************************
-Name of the task:
-****************************/
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-const NAME_FIELD = document.getElementById("nameField");
-const AGE_FIELD = document.getElementById("ageField");
-
-
 console.log("Running t12_conditionals.js")
 
+let age = 15;
 
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-
-
-/****************************
-Main code
-****************************/
-
-
-
-
-/****************************
-Functions
-****************************/
-function getFormInput(){
-    let userName = NAME_FIELD.value;
-    let userAge = AGE_FIELD.value;
-
-    OUTPUT.innerHTML ="<h2>Added by JavaScript</h2>";
-    OUTPUT.innerHTML +="<p>Your name is " + userName + "</p>";
-    OUTPUT.innerHTML +="<p>Your age is " + userAge + "</p>";
-    OUTPUT.innerHTML +="<p>In 10 years you will be " + (Number(userAge) + 10) + "</p>";
+if(age<70){
+    console.log("Definitely too young");
 }
