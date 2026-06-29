7c56ced97730463e58a903778f4409f9a84958f2
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-29 23:39:02 +0000

    Jun 29, 2026, 11:39 PM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -1,1 +1,4 @@
-/* Paste your code from the last task */
+let classArray = ["Airi", "Lily", "Ella", "Isabella", "Emily", "Olivia"];
+OUTPUT.innerHTML = "The sixth person in the class is " +classArray[6];
+
+
