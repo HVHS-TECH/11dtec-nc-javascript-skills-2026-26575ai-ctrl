e423eb58fbeee206e0474c4341d420b4584acd97
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-06-30 00:04:57 +0000

    Jun 30, 2026, 12:04 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -2,7 +2,6 @@ console.log("Running t14_arrays_1.js")
 
 let classArray = ["Airi", "Lily", "Ella", "Isabella", "Emily", "Olivia"];
 let choice = CHOOSE_FIELD.value;
-document.getElementById("spaceForJavaScriptOutput");
-if(clickedButton === "submit"){
-  OUTPUT.innerHTML = "You chose: " + classArray[choice] + "<br>";
-}
+document.getElementById("spaceForJavaScriptOutput").addEventListener("click", function(){
+    alert("You chose: " + classArray[choice]);
+});
