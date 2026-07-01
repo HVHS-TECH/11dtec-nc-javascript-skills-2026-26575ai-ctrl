bff853be67feba70da025ae9c7201c772508d3d8
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-07-01 02:32:12 +0000

    Jul 1, 2026, 2:32 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -1,5 +1,9 @@
-let names = ["Airi", "Lily", "Ella", "Isabella", "Emily", "Olivia"];
-document.getElementById("submit").addEventListener("click", function(){
-    let number = document.getElementById("number").value;
+function getFormInput(event){
+    event.preventDefault();
+    let number = Number(document.getElementById("number").value);
+    let names = ["Airi", "Lily", "Ella", "Isabella", "Emily", "Olivia"];
     document.getElementById("result").textContent = names[number];
-});
+}
+
+
+
