b8e7866b9bbb3cfc41f5acbbed75e314cdff1667
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-07-20 23:45:48 +0000

    Jul 20, 2026, 11:45 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,7 +1,8 @@
-const OUTPUT = document.getElementById("t15_for_loop");
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 console.log("Starting Loop");
 for(count=0;count<10;count++){
     console.log("Current Count: " + count);
+    OUTPUT.innerHTML += "Current Count: " + count + "<br>";
 }
 
 console.log("Loop stopped");    
