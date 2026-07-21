ccf74b5941c15c8588b56e364f7b3288d407b290
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-07-21 00:05:25 +0000

    Jul 21, 2026, 12:05 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -3,7 +3,7 @@ console.log("t16_arrays_continued.js loaded");
 function getFormInput(event){
     event.preventDefault();
     let number = Number(document.getElementById("number").value);
-    let names = ["Airi", "Lily", "Ella", "Isabella", "Emily", "Olivia"];
+
     OUTPUT.innerHTML += "Person " + (classArray.length ) + ": " + classArray[classArray.length - 1] + "<br>";
 }
 console.log("Starting Loop");
