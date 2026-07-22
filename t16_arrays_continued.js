6f913ce53c7da5d96a86bbc2cd7b9479c34a513e
26575ai-ctrl <26575ai@hvhs.school.nz>
2026-07-22 02:27:11 +0000

    Jul 22, 2026, 2:27 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -1,5 +1,8 @@
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 console.log("t16_arrays_continued.js loaded");
 
-const names = ["Airi", "Lily", "Ella", "Isabella", "Emily", "Olivia"];
+var names = ["Airi", "Lily", "Ella", "Isabella", "Emily", "Olivia"];
+var classArray = [];
+console.log(names);
+console.log(classArray);
 classArray.push("Anand");
